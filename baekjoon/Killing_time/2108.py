8c8999669d3320a7185c8078f396d1eda89df507
Hyeonu Yun <yhw8258@naver.com>
2020-03-30 12:03:42 +0900

    200330 극한의 숏코더로부터 레퍼런스 적어봄

--- baekjoon/Killing_time/2108.py
+++ baekjoon/Killing_time/2108.py
@@ -90,28 +90,38 @@ else:
 
 print(max(lst)-min(lst)) # 범위
 """
-import sys
-from collections import Counter
-n = int(input())
-lst = []
-for i in range(n):
-    x = int(sys.stdin.readline())
-    lst.append(x)
-lst.sort()
+# import sys
+# from collections import Counter
+# n = int(input())
+# lst = []
+# for i in range(n):
+#     x = int(sys.stdin.readline())
+#     lst.append(x)
+# lst.sort()
 
-x = Counter(lst).most_common()
-tmp = [x[0][0]]
-i = 0
-while i < len(x)-1 and x[i][1] == x[i+1][1]:
-    tmp.append(x[i+1][0])
-    i += 1
+# x = Counter(lst).most_common()
+# tmp = [x[0][0]]
+# i = 0
+# while i < len(x)-1 and x[i][1] == x[i+1][1]:
+#     tmp.append(x[i+1][0])
+#     i += 1
 
-print('%.0f'% (sum(lst)/len(lst))) # 산술평균
-print(lst[n//2]) # 중앙값
+# print('%.0f'% (sum(lst)/len(lst))) # 산술평균
+# print(lst[n//2]) # 중앙값
 
-if len(tmp) == 1:
-    print(tmp[0])
-else:
-    print(sorted(tmp)[1])
+# if len(tmp) == 1:
+#     print(tmp[0])
+# else:
+#     print(sorted(tmp)[1])
 
-print(max(lst)-min(lst)) # 범위
+# print(max(lst)-min(lst)) # 범위
+
+
+# 다른사람 중 극한의 숏코더 것을 참고했는데
+# 특히 126번과 127번 라인에서 최빈값 쪽이 신기했다. 저런 식으로도 할 수 있구나.
+# 진심 혼자서 계속 보면서 이게 대체 뭔가 싶었다.
+# import sys, collections
+# d = sorted(int(sys.stdin.readline()) for i in "0" * int(input()))
+# c = collections.Counter(d)
+# t = sorted(x for x in c if c[x] == max(c.values()))
+# print('%.f' % (sum(d)/len(d)), d[len(d)//2], t[len(t)!=1], max(d)-min(d), sep='\n')
