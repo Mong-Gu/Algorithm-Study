0ba19577851d21c7dc01a876b906e85c5304045b
Hyeonu Yun <yhw8258@naver.com>
2020-04-01 23:52:45 +0900

    200401 그리디 푸는중

--- baekjoon/Greedy/1115.py
+++ baekjoon/Greedy/1115.py
@@ -1,38 +1,43 @@
 from itertools import permutations
 
 n = int(input())
-target = [int(x) for x in input().split(' ')]
+P = [int(x) for x in input().split(' ')]
 
-A = list(permutations(range(n), n))
-Perfect = []
+A = list(permutations(range(n), n)) # 순열들을 모아둔 리스트
+B = [] # A의 각 원소에 위치한 순열들을 돌며 자식 순열을 만들어보기 위함
+Perfect = [] # 그중 완벽한 순열인 것들만 모아둔 리스트
+
+# print(A)
 
 for i in range(len(A)):
-    B = []
-    B.append(0)
+    B = [0]
     for j in range(1, n):
-        tmp = A[i][B[j-1]]
-        if tmp in B:
+        x = A[i][B[j-1]]
+        if x in B:
             break
         else:
-            B.append(tmp)
+            B.append(x)
     else:
-        Perfect.append(A[i])
+        Perfect.append(list(A[i]))
+        Perfect.append(B)
 
-print(Perfect)
+print (Perfect)
+# 중복되는 값들을 제거해야 하는데 리스트로 담았다 보니까 set이 안먹힘
 
 
-best = 9999
+# best = 9999
 
-for i in range(len(Perfect)):
-    tmp = 0
-    for j in range(n):
-        if target[j] != Perfect[i][j]:
-            tmp += 1
-            if tmp >= best:
-                break
-    if best > tmp:
-        best = tmp
+# for i in range(len(Perfect)):
+#     tmp = 0
+#     for j in range(n):
+#         if target[j] != Perfect[i][j]:
+#             tmp += 1
+#             if tmp >= best:
+#                 break
+#     if best > tmp:
+#         best = tmp
 
-print(best)
+# print(best)
 
-# 이게 왜 메모리 터지지?
+# # 이게 왜 메모리 터지지?
+
