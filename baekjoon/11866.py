b74a0ce290fa5bdc031b41f4c119a8853504847d
Mong-Gu <yhw8258@naver.com>
2020-06-23 14:21:23 +0900

    200623 놀러 나가기 전에 잠깐!

--- baekjoon/11866.py
+++ baekjoon/11866.py
@@ -1,10 +1,13 @@
 from collections import deque
+
 n, k = map(int, input().split())
 d = deque([i for i in range(1, n+1)])
 lst = []
+
 while d:
     d.rotate(-(k-1))
     lst.append(d.popleft())
+
 print('<', end = '')
 for i in range(len(lst)):
     if i != len(lst)-1:
